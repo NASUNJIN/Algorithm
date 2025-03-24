874ad60365b0fed3571bcb9aa7de479699948163
NASUNJIN <sj010618@gmail.com>
2025-03-24 11:58:26 +0900

    dp: 2025.03.24

--- BAEKJOON/SILVER/[BOJ_S1] 1309_동물원.py
+++ BAEKJOON/SILVER/[BOJ_S1] 1309_동물원.py
@@ -13,13 +13,13 @@ N = int(stdin.readline())
 
 def solution(N):
   if N == 1 :
-    print(3)
+    return(3)
   else :
     dp = [1 for _ in range(N+1)]
     dp[1], dp[2] = 3, 7
     
     for i in range(3, N + 1):
       dp[i] = (2 * dp[i-1] + dp[i-2]) % 9901
-    print(dp[N])
+    return(dp[N])
 
-solution(N)
+print(solution(N))
