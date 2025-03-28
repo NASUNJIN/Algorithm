4223197703d763793df051217e8d0068813bb184
NASUNJIN <sj010618@gmail.com>
2025-03-28 10:53:58 +0900

    dp: 2025.03.28

---
+++ PROGRAMMERS/Lv3/[연습문제] 43105_정수 삼각형.py
@@ -0,0 +1,18 @@
+# 아래층에서 올라가기
+def solution(triangle):
+    
+  N = len(triangle) - 1
+
+  while N > 0:  # N, N-1, ... 1
+    print(N)
+    for i in range(N):
+      # 바로 윗층칸에 아래칸 두개중 큰 값 더함
+      triangle[N-1][i] += max(triangle[N][i], triangle[N][i+1])
+    # 층 하나 올리기
+    N -=1
+  
+  return triangle[0][0]
+
+
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+print(solution(triangle))
